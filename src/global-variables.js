edf8ddb67c27675e7d8ad0150dcc7570d95b87cd
doremansunio <136595579+doremansunio@users.noreply.github.com>
2023-12-05 08:15:28 +0530

    Update global-variables.js

--- src/global-variables.js
+++ src/global-variables.js
@@ -1,8 +1,8 @@
 // Those variables should be filled out with your AWS & Cognito User Pool configurations.
-export const REGION = '** place your region name in string. **';
+export const REGION = 'us-east-1';
 export const POOL_DATA = {
-  UserPoolId: '** place your Pool Id. **',
-  ClientId: '** place your App Client Id. **'
+  UserPoolId: 'us-east-1_luounRzv0',
+  ClientId: '7rbfge7ic1s47l8icu8jo3ht0f'
 };
 export const IDENTITY_POOL_ID = '** place your Cognito Identity Pool Id **';
 export const LOGINS_KEY = 'cognito-idp.' + REGION + '.amazonaws.com/' + POOL_DATA.UserPoolId;
